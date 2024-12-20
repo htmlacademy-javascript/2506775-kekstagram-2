ec3095af25d5f489699f3d96b1b1a8b2d129806a
Nargiza <nargizusya@gmail.com>
2024-12-20 20:09:23 +0500

    feat: добавляет функции для id

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+const photoPublished = 25;
+
 const NAMES = ['София', 'Артем', 'Ева', 'Амина', 'Давид', 'Максим', 'Дарина', 'Алия', 'Амелия', 'Дамир', 'Арслан'];
 
 const MESSAGES = [
@@ -9,7 +11,14 @@ const MESSAGES = [
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 
-const getRandomIndex = (array) => Math.floor(Math.random() * array.length);
+const idGenerator = () => {
+  let currentId = 0;
+  return () => ++currentId;
+};
+
+const getId = idGenerator();
+const getPhoto = idGenerator();
+const getIdComment = idGenerator();
 
 const getRandomInteger = (a, b) => {
   const lower = Math.ceil(Math.min(a, b));
@@ -18,45 +27,23 @@ const getRandomInteger = (a, b) => {
   return Math.floor(result);
 };
 
-const photosArray = [];
-
-//Пыталась вывести неповторяющееся число из массива
-
-// const intArray = [];
-
-// const indexId = (a, b) => {
-//   for(let i = 25; i > intArray.length; i--){
-//     if (!intArray.includes(getRandomInteger(a, b))){
-//       intArray.push(getRandomInteger(a, b));
-//     }
-//   }
-//   return getRandomInteger(a, b);
-// };
-// console.log(indexId(1,25));
-// console.log(intArray);
+const commentGenerator = () => {
+  const comment = {
+    id:getIdComment(),
+    avatar:`img/avatar-${getRandomInteger(1, 6)}`,
+    message:MESSAGES[getRandomInteger(0, MESSAGES.length - 1)],
+    name:NAMES[getRandomInteger(0, NAMES.length - 1)]
+  };
+  return comment;
+};
 
 const photoDescription = () => ({
-  id:getRandomInteger(1, 25),
-  url:`photo/${ getRandomInteger(1, 25) }.jpg`,
+  id:getId(),
+  url:`photo/${ getPhoto() }.jpg`,
   description:'Моменты, которые запечатлены навсегда',
   likes:getRandomInteger(15, 200),
-  comments:[
-    {
-      id:getRandomInteger(15, 200),
-      avatar:`img/avatar-${getRandomInteger(1, 6)}`,
-      message:MESSAGES[getRandomIndex(MESSAGES)],
-      name:NAMES[getRandomIndex(NAMES)]
-    }
-  ],
+  comments: Array.from({length: getRandomInteger(0, 30)}, () => commentGenerator())
 });
 
-for(let i = 0; i < 25; i++){
-  photosArray.push(photoDescription());
-}
-console.log(photosArray);
-
-//id, число — идентификатор опубликованной фотографии. Это число от 1 до 25. Идентификаторы не должны повторяться.?
-//url, строка — адрес картинки вида photos/{{i}}.jpg, где {{i}} — это число от 1 до 25. Адреса картинок не должны повторяться.?
-//У каждого комментария есть идентификатор — id — любое число. Идентификаторы не должны повторяться.?
-//comments, массив объектов ?
+const photoArray = Array.from({length: photoPublished}, () => photoDescription());
 
