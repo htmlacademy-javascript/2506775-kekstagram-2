ef5fcca96dcf17d34828f4a37389a2d7b3fbadb9
Keks <keks@htmlacademy.ru>
2024-12-25 17:28:11 +0300

    :heavy_check_mark: Сборка #4

--- 4/js/main.js
+++ 4/js/main.js
@@ -1,7 +1,16 @@
-const photoPublished = 25;
+const PHOTO_PUBLISHED_COUNT = 25;
+const PHOTO_DESCRIPTION = 'Моменты, которые запечатлены навсегда';
+
+const AVATAR_COUNT_FROM = 1;
+const AVATAR_COUNT_TO = 6;
+
+const LIKES_COUNT_FROM = 15;
+const LIKES_COUNT_TO = 200;
+
+const COMMENTS_COUNT_FROM = 0;
+const COMMENTS_COUNT_TO = 30;
 
 const NAMES = ['София', 'Артем', 'Ева', 'Амина', 'Давид', 'Максим', 'Дарина', 'Алия', 'Амелия', 'Дамир', 'Арслан'];
-
 const MESSAGES = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
@@ -11,15 +20,6 @@ const MESSAGES = [
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 
-const idGenerator = () => {
-  let currentId = 0;
-  return () => ++currentId;
-};
-
-const getId = idGenerator();
-const getPhoto = idGenerator();
-const getIdComment = idGenerator();
-
 const getRandomInteger = (a, b) => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
@@ -27,24 +27,34 @@ const getRandomInteger = (a, b) => {
   return Math.floor(result);
 };
 
-const commentGenerator = () => {
+const getId = () => {
+  let currentId = 0;
+  return () => ++currentId;
+};
+
+const getPhotoId = getId();
+const getCommentId = getId();
+
+const getComments = () => {
   const comment = {
-    id:getIdComment(),
-    avatar:`img/avatar-${getRandomInteger(1, 6)}`,
+    id:getCommentId(),
+    avatar:`img/avatar-${getRandomInteger(AVATAR_COUNT_FROM, AVATAR_COUNT_TO)}`,
     message:MESSAGES[getRandomInteger(0, MESSAGES.length - 1)],
     name:NAMES[getRandomInteger(0, NAMES.length - 1)]
   };
   return comment;
 };
 
-const photoDescription = () => ({
-  id:getId(),
-  url:`photo/${ getPhoto() }.jpg`,
-  description:'Моменты, которые запечатлены навсегда',
-  likes:getRandomInteger(15, 200),
-  comments: Array.from({length: getRandomInteger(0, 30)}, () => commentGenerator())
-});
+const getPhotoDescription = () => {
+  const id = getPhotoId();
+  return {
+    id: id,
+    url:`photo/${id}.jpg`,
+    description:PHOTO_DESCRIPTION,
+    likes:getRandomInteger(LIKES_COUNT_FROM,LIKES_COUNT_TO),
+    comments: Array.from({length:getRandomInteger(COMMENTS_COUNT_FROM, COMMENTS_COUNT_TO)}, getComments)
+  };
+};
+export const getPhotos = (photoCount = PHOTO_PUBLISHED_COUNT) => Array.from({length:photoCount}, getPhotoDescription);
 
-const photoArray = Array.from({length: photoPublished}, () => photoDescription());
 
-console.log(photoArray);
