ce2c6a680aa264c94ce51bce6f96d043e8d4da4a
Keks <keks@htmlacademy.ru>
2025-01-11 21:09:55 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/functions.js
@@ -0,0 +1,59 @@
+const checkLength = (string, length) => string.length <= length;
+
+checkLength('проверяемая строка', 18);
+
+
+const isPalindromeString = (string) => {
+  const newString = string.replaceAll(' ','').toLowerCase();
+
+  let invertedString = '';
+  for(let i = newString.length - 1; i >= 0; i--){
+    invertedString += newString[i];
+  }
+
+  return newString === invertedString;
+
+};
+
+isPalindromeString('Лёша на полке клопа нашёл ');
+
+const getNumber = (string) => {
+  const replacedString = string.replaceAll(' ','');
+  let numberString = '';
+
+  for(let i = 0; i <= replacedString.length - 1; i++){
+    if(!isNaN(replacedString[i])){
+      numberString += replacedString[i];
+    }
+  }
+
+  return parseInt(numberString, 10);
+};
+
+getNumber('ECMAScript 2022');
+
+
+// Напишите функцию, которая принимает время начала и конца рабочего дня, а также время старта и продолжительность встречи в минутах и возвращает true, если встреча не выходит за рамки рабочего дня, и false, если выходит.
+
+// Время указывается в виде строки в формате часы:минуты. Для указания часов и минут могут использоваться как две цифры, так и одна. Например, 8 часов 5 минут могут быть указаны по-разному: 08:05, 8:5, 08:5 или 8:05.
+
+// Продолжительность задаётся числом. Гарантируется, что и рабочий день, и встреча укладываются в одни календарные сутки.
+
+function parseTime(time){
+  const [hours, minutes] = time.split(':').map(Number);
+  return hours * 60 + minutes;
+}
+
+const isMeeting = (startWork, endWork, startMeeting, duration) => {
+  const workStart = parseTime(startWork);
+  const workEnd = parseTime(endWork);
+  const meetingStart = parseTime(startMeeting);
+
+  const meetingEnd = meetingStart + duration;
+
+  const isTimeOfMeeting = meetingStart >= workStart && meetingEnd <= workEnd;
+
+  return isTimeOfMeeting;
+};
+
+isMeeting('14:00', '17:30', '08:0', 90);
---
+++ 6/js/main.js
@@ -0,0 +1,3 @@
+import { getPhotos } from './modules/photo-desc';
+
+getPhotos();
