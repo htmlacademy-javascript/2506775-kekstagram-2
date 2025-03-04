f3ddfdc085ceeb091741cfc7122d6caf4e5e6731
Nargiza <nargizusya@gmail.com>
2025-03-04 06:45:13 +0500

    feat:добавляет функциии открытия и закрытия поля ввода

---
+++ js/modules/upload-form.js
@@ -0,0 +1,33 @@
+import { isEscapeKey } from './util';
+
+const formUpload = document.querySelector('.img-upload__form');
+const imageUploadInput = formUpload.querySelector('.img-upload__input');
+const imageOverlay = formUpload.querySelector('.img-upload__overlay');
+const imageUploadCancle = formUpload.querySelector('.img-upload__cancel');
+const uploadHashtag = formUpload.querySelector('.text__hashtags');
+
+const pristine = new Pristine(formUpload);
+
+const onDocumentKeyDown = (evt) => {
+  if(isEscapeKey(evt)){
+    evt.preventDefault();
+    closeUploadWindow();
+  }
+};
+
+const openUploadWindow = () => {
+  imageOverlay.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+  document.addEventListener('keydown', onDocumentKeyDown);
+};
+
+function closeUploadWindow() {
+  imageUploadInput.value = '';
+  imageOverlay.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onDocumentKeyDown);
+}
+
+imageUploadInput.addEventListener('change', openUploadWindow);
+
+imageUploadCancle.addEventListener('click', closeUploadWindow);
