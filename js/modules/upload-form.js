d19af5a004041e69fd13e5ec8d66edafdcea3615
Nargiza <nargizusya@gmail.com>
2025-03-15 00:21:28 +0500

    feat: применение эффектов к изображению

--- js/modules/upload-form.js
+++ js/modules/upload-form.js
@@ -14,6 +14,14 @@ const errorMessage = {
   commentsLength: 'длина комментария больше 140 символов'
 };
 
+const effectKeys = {
+  chrome: 'grayscale',
+  sepia: 'sepia',
+  marvin: 'invert',
+  phobos: 'blur',
+  heat: 'brightness',
+};
+
 const formUpload = document.querySelector('.img-upload__form');
 const imageUploadInput = formUpload.querySelector('.img-upload__input');
 const imageOverlay = formUpload.querySelector('.img-upload__overlay');
@@ -24,12 +32,40 @@ const imgUploadPrewiew = formUpload.querySelector('.img-upload__preview img');
 const scaleControlSmaller = formUpload.querySelector('.scale__control--smaller');
 const scaleControlBigger = formUpload.querySelector('.scale__control--bigger');
 const scaleControl = formUpload.querySelector('.scale__control--value');
+const slider = formUpload.querySelector('.effect-level__slider');
+const effectValue = formUpload.querySelector('.effect-level__value');
+const effectLevel = formUpload.querySelector('.img-upload__effect-level');
+const effectList = formUpload.querySelector('.effects__list');
+
+effectLevel.classList.add('hidden');
+
 
 const pristine = new Pristine(formUpload, {
   classTo: 'img-upload__field-wrapper',
   errorTextParent: 'img-upload__field-wrapper',
   errorTextTag: 'div',
   errorTextClass: 'img-upload__field-wrapper--error'
+});
+
+noUiSlider.create(slider, {
+  range: {
+    min: 0,
+    max: 1,
+  },
+  start: 0,
+  step: 0.1,
+  connect: 'lower',
+  format: {
+    to: function (value) {
+      if (Number.isInteger(value)) {
+        return value.toFixed(0);
+      }
+      return value.toFixed(1);
+    },
+    from: function (value) {
+      return parseFloat(value);
+    },
+  },
 });
 
 const onDocumentKeyDown = (evt) => {
@@ -123,3 +159,68 @@ formUpload.addEventListener('submit', (evt) => {
 
 scaleControlSmaller.addEventListener('click', onZoomOut);
 scaleControlBigger.addEventListener('click', onZoomIn);
+
+effectList.addEventListener('change', (evt) => {
+  effectLevel.classList.remove('hidden');
+  const effect = evt.target.value;
+
+  slider.noUiSlider.on('update', () => {
+    effectValue.value = slider.noUiSlider.get();
+    console.log(effectKeys.value);
+  });
+
+  if(effect === 'none'){
+    effectLevel.classList.add('hidden');
+    imgUploadPrewiew.style.filter = 'none';
+  }else if(effect === 'chrome') {
+    slider.noUiSlider.updateOptions({
+      range: {
+        min: 0,
+        max: 1
+      },
+      start: 1,
+      step: 0.1
+    });
+    imgUploadPrewiew.style.filter = `${effectKeys.chrome}(${effectValue.value})`;
+  } else if(effect === 'sepia') {
+    slider.noUiSlider.updateOptions({
+      range: {
+        min: 0,
+        max: 1
+      },
+      start: 1,
+      step: 0.1
+    });
+    imgUploadPrewiew.style.filter = `${effectKeys.sepia}(${effectValue.value})`;
+  } else if(effect === 'marvin'){
+    slider.noUiSlider.updateOptions({
+      range: {
+        min: 0,
+        max: 100,
+      },
+      start: 100,
+      step: 1
+    });
+    imgUploadPrewiew.style.filter = `${effectKeys.marvin}(${effectValue.value}%)`;
+  } else if(effect === 'phobos'){
+    slider.noUiSlider.updateOptions({
+      range: {
+        min: 0,
+        max: 3,
+      },
+      start: 3,
+      step: 0.1,
+    });
+    imgUploadPrewiew.style.filter = `${effectKeys.phobos}(${effectValue.value}px)`;
+  } else if(effect === 'heat'){
+    slider.noUiSlider.updateOptions({
+      range: {
+        min: 1,
+        max: 3,
+      },
+      start: 3,
+      step: 0.1,
+    });
+    imgUploadPrewiew.style.filter = `${effectKeys.heat}(${effectValue.value})`;
+  }
+});
