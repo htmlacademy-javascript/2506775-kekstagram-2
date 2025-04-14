7f78ab96602930787af52a36bdf7a16d2d875862
Nargiza <nargizusya@gmail.com>
2025-04-14 21:31:58 +0500

    ашчЖ удаляет id

--- js/modules/util.js
+++ js/modules/util.js
@@ -11,19 +11,6 @@ const keysToProcess = {
   Escape: 'Escape'
 };
 
-// const getRandomInteger = (a, b) => {
-//   const lower = Math.ceil(Math.min(a, b));
-//   const upper = Math.floor(Math.max(a, b));
-//   const result = Math.random() * (upper - lower + 1) + lower;
-//   return Math.floor(result);
-// };
-
-
-// const getId = () => {
-//   let currentId = 0;
-//   return () => ++currentId;
-// };
-
 const isEscapeKey = (evt) => evt.key === keysToProcess.Escape;
 
 const showSuccess = () => {
